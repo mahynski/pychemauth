df34345360a4f287eb3f5f4bac99ae7af0772364
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2021-12-15 22:48:05 -0500

    updated simca DoF estimators

--- classifier/simca.py
+++ classifier/simca.py
@@ -559,8 +559,17 @@ class DDSIMCA(ClassifierMixin, BaseEstimator):
     def estimate_dof_(self, h_vals, q_vals):
         """Estimate the degrees of freedom for the chi-squared distribution."""
 
-        def err2(N, vals):  # Use "robust" method for estimating DoF
-            x0 = np.mean(vals)
+        def err2(N, vals):
+            """
+            Use a "robust" method for estimating DoF.
+
+            In [1] Eq. 14 suggests the IQR should be divided by the mean (h0),
+            however, the citation they provide suggests the median might be
+            a better choice; in practice, it seems that is favored since it
+            is more robust against outliers, so this is used below in that
+            spirit.
+            """
+            x0 = np.median(vals)  # np.mean(vals)
             a = (
                 scipy.stats.chi2.ppf(0.75, N) - scipy.stats.chi2.ppf(0.25, N)
             ) / N
@@ -573,9 +582,11 @@ class DDSIMCA(ClassifierMixin, BaseEstimator):
             err2, self.n_components, args=(h_vals), method="Nelder-Mead"
         )
         if res.success:
+            # Robust method, if possible
             Nh = res.x[0]
         else:
-            raise Exception("Could not compute N_h : {}".format(res.message))
+            # Use simple estimate if this fails (Eq. 13 in [1])
+            Nh = 2.0 * np.mean(h_vals) ** 2 / np.std(h_vals, ddof=1) ** 2
 
         # As in conclusions of [1], Nq ~ rank(X)-n_components is expected;
         # assuming near full rank then this is min(I,J)-n_components
@@ -587,9 +598,11 @@ class DDSIMCA(ClassifierMixin, BaseEstimator):
             method="Nelder-Mead",
         )
         if res.success:
+            # Robust method, if possible
             Nq = res.x[0]
         else:
-            raise Exception("Could not compute N_q : {}".format(res.message))
+            # Use simple estimate if this fails (Eq. 23 in [1])
+            Nq = 2.0 * np.mean(q_vals) ** 2 / np.std(q_vals, ddof=1) ** 2
 
         return Nh, Nq
 
