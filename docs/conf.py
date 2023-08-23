5176141a615f7c5c599c858f0ba05d55137e3573
Nathan Mahynski <nathan.mahynski@gmail.com>
2023-08-23 12:52:18 -0400

    fixed docf

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'jupyter']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
