2f12734820eb712fdda599754b8cbb412671760e
Nathan Mahynski <nathan.mahynski@gmail.com>
2023-09-13 15:17:13 -0400

    precommit clean up

--- pychemauth/datasets.py
+++ pychemauth/datasets.py
@@ -3,11 +3,10 @@ Load datasets.
 
 author: nam
 """
-import requests
 import io
 
 import pandas as pd
-
+import requests
 from sklearn.utils import Bunch
 
 
@@ -32,10 +31,10 @@ def load_pgaa(return_X_y=False, as_frame=False):
     data : Bunch
         Dictionary-like object, with the following attributes.
 
-        data{ndarray, DataFrame} 
+        data{ndarray, DataFrame}
             The data matrix. If as_frame=True, data will be a pandas DataFrame.
 
-        target: {ndarray, Series} 
+        target: {ndarray, Series}
             The classification target. If as_frame=True, target will be a pandas Series.
 
         feature_names: list
@@ -51,7 +50,7 @@ def load_pgaa(return_X_y=False, as_frame=False):
             The full description of the dataset.
 
     (data, target) : tuple if return_X_y is True
-        A tuple of two ndarrays by default. The first contains a 2D array 
+        A tuple of two ndarrays by default. The first contains a 2D array
         with each row representing one sample and each column representing the features.
         The second array contains the target samples.
 
@@ -68,19 +67,25 @@ def load_pgaa(return_X_y=False, as_frame=False):
     (2023). https://doi.org/10.1007/s10967-023-09024-x
     """
     # Load data directly from the web so this will reflect any future changes.
-    url = "https://raw.githubusercontent.com/mahynski/pgaa-material-authentication/master/data/raw/centers.csv" 
-    centers = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), dtype=float)
+    url = "https://raw.githubusercontent.com/mahynski/pgaa-material-authentication/master/data/raw/centers.csv"
+    centers = pd.read_csv(
+        io.StringIO(requests.get(url).content.decode("utf-8")), dtype=float
+    )
     feature_names = [name[0] for name in centers.values.tolist()]
 
-    url = "https://raw.githubusercontent.com/mahynski/pgaa-material-authentication/master/data/raw/X.csv" 
-    X = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), dtype=float)
+    url = "https://raw.githubusercontent.com/mahynski/pgaa-material-authentication/master/data/raw/X.csv"
+    X = pd.read_csv(
+        io.StringIO(requests.get(url).content.decode("utf-8")), dtype=float
+    )
     X.columns = feature_names
     if not as_frame:
         X = X.values
 
     target_names = ["Material"]
-    url = "https://raw.githubusercontent.com/mahynski/pgaa-material-authentication/master/data/raw/y.csv" 
-    y = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), dtype=str).squeeze()
+    url = "https://raw.githubusercontent.com/mahynski/pgaa-material-authentication/master/data/raw/y.csv"
+    y = pd.read_csv(
+        io.StringIO(requests.get(url).content.decode("utf-8")), dtype=str
+    ).squeeze()
     y.name = target_names[0]
     if not as_frame:
         y = y.values
@@ -99,7 +104,7 @@ Classification and authentication of materials using prompt gamma ray activation
 Radioanal Nucl Chem 332, 3259–3271 (2023). https://doi.org/10.1007/s10967-023-09024-x . \
 See this publication for a full description. Briefly, rows of X are PGAA spectra for different \
 materials. They have been normalized to sum to 1. The peaks have be binned into histograms whose \
-centers (energy in keV) are given as the feature_names. y contains the name of each material."
+centers (energy in keV) are given as the feature_names. y contains the name of each material.",
         )
         return bunch
 
@@ -125,10 +130,10 @@ def load_stamp2010(return_X_y=False, as_frame=False):
     data : Bunch
         Dictionary-like object, with the following attributes.
 
-        data{ndarray, DataFrame} 
+        data{ndarray, DataFrame}
             The data matrix. If as_frame=True, data will be a pandas DataFrame.
 
-        target: {ndarray, DataFrame} 
+        target: {ndarray, DataFrame}
             The classification target. If as_frame=True, target will be a pandas DataFrame.
 
         feature_names: list
@@ -137,7 +142,7 @@ def load_stamp2010(return_X_y=False, as_frame=False):
         target_names: list
             The names of target classes.
 
-        frame: DataFrame 
+        frame: DataFrame
             Only present when as_frame=True. DataFrame with data and target.
 
         DESCR: str
@@ -167,14 +172,16 @@ def load_stamp2010(return_X_y=False, as_frame=False):
     Science & Technology 56.20 (2022): 14361-14374.
     """
     # Load data directly from the web so this will reflect any future changes.
-    url = "https://raw.githubusercontent.com/mahynski/stamp-dataset-1999-2010/master/X.csv" 
-    X = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')), dtype=float)
+    url = "https://raw.githubusercontent.com/mahynski/stamp-dataset-1999-2010/master/X.csv"
+    X = pd.read_csv(
+        io.StringIO(requests.get(url).content.decode("utf-8")), dtype=float
+    )
     feature_names = X.columns.tolist()
     if not as_frame:
         X = X.values
 
-    url = "https://raw.githubusercontent.com/mahynski/stamp-dataset-1999-2010/master/y.csv" 
-    y = pd.read_csv(io.StringIO(requests.get(url).content.decode('utf-8')))
+    url = "https://raw.githubusercontent.com/mahynski/stamp-dataset-1999-2010/master/y.csv"
+    y = pd.read_csv(io.StringIO(requests.get(url).content.decode("utf-8")))
     target_names = y.columns.tolist()
     if not as_frame:
         y = y.values
@@ -191,6 +198,6 @@ def load_stamp2010(return_X_y=False, as_frame=False):
             DESCR="Dataset reported in Schuur, Stacy S., Ragland, Jared M., Mahynski, \
 Nathan A. (2021), Data Supporting 'Seabird Tissue Archival and Monitoring Project \
 (STAMP) Data from 1999-2010', National Institute of Standards and Technology, \
-https://doi.org/10.18434/mds2-2431 . See this publication for a full description."
+https://doi.org/10.18434/mds2-2431 . See this publication for a full description.",
         )
         return bunch
