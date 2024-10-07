4c92c43a56146c778bf86b3dacea3af5cb03eb2a
Nathan Mahynski <nathan.mahynski@gmail.com>
2024-10-07 09:24:34 -0400

    type checked feature_selection.py

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
     ],
     install_requires=[
-        "mypy==1.11.2", # For type hints and checking
-        "types-requests==2.32.0.20240914", # For type checking
+        "mypy==1.11.2",  # For type hints and checking
+        "types-requests==2.32.0.20240914",  # For type checking
         "torch",  # For DIME - just need torch tensor libraries so no need for GPU support which can also cause conflicts
         "dime-pytorch==1.0.1",
         "baycomp==1.0.3",
