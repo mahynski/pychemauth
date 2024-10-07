99daa32dd7b2d1865cc9ca66f49a899baf6581c0
Nathan Mahynski <nathan.mahynski@gmail.com>
2024-10-07 10:13:25 -0400

    type checked iumbalanced.py

--- pychemauth/preprocessing/imbalanced.py
+++ pychemauth/preprocessing/imbalanced.py
@@ -3,10 +3,15 @@ Dealing with imbalanced datasets.
 
 author: nam
 """
+import numpy as np
+
 from imblearn.combine import SMOTEENN
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import EditedNearestNeighbours as ENN
 from sklearn.preprocessing import StandardScaler
+
+from typing import Any, Union, ClassVar
+from numpy.typing import NDArray
 
 
 class ScaledSMOTEENN:
@@ -40,23 +45,13 @@ class ScaledSMOTEENN:
     ----
     See imblearn's documentation for details on these parameters.
 
-    This uses the imblearn library to perform SMOTE-ENN,
-    that is, SMOTE followed by edited nearest neighbors (ENN).
-    This is done in a way that is compatible with
-    imblearn's estimator API.  This class performs
-    (1) Scaling, then (2) SMOTE-ENN, then
-    (3) de-Scales to return a dataset in the
-    original "units" provided.  The scaler can be chosen.
+    This uses the imblearn library to perform SMOTE-ENN, that is, SMOTE followed by edited nearest neighbors (ENN). This is done in a way that is compatible with imblearn's estimator API.  This class performs (1) Scaling, then (2) SMOTE-ENN, then (3) de-Scales to return a dataset in the original "units" provided.  The scaler can be chosen.
 
     Warning
     -------
-    This is designed to work with **imblearn.pipeline** not sklearn.pipeline;
-    they are often interchangeable, but not in this case.  Be sure to
-    instantiate imblearn's version as in the example below.
+    This is designed to work with **imblearn.pipeline** not sklearn.pipeline; they are often interchangeable, but not in this case.  Be sure to instantiate imblearn's version as in the example below.
 
-    This is because imblearn's pipelines intermediates allow fit,
-    transform, and resample methods; however, samplers are only
-    applied during the fitting stage (training).
+    This is because imblearn's pipelines intermediates allow fit, transform, and resample methods; however, samplers are only applied during the fitting stage (training).
 
     References
     ---------
@@ -82,16 +77,24 @@ class ScaledSMOTEENN:
     >>> gs.fit(X_imbalanced_train, y_imbalanced_train)
     """
 
+    sampling_strategy_smote: ClassVar[str]
+    sampling_strategy_enn: ClassVar[str]
+    k_smote: ClassVar[int]
+    k_enn: ClassVar[int]
+    kind_sel_enn: ClassVar[str]
+    random_state: ClassVar[int]
+    scaler: ClassVar[Any]
+
     def __init__(
         self,
-        sampling_strategy_smote="not majority",
-        sampling_strategy_enn="not minority",
-        k_smote=5,
-        k_enn=3,
-        kind_sel_enn="all",
-        random_state=0,
-        scaler=StandardScaler(with_mean=True, with_std=True),
-    ):
+        sampling_strategy_smote: str = "not majority",
+        sampling_strategy_enn: str = "not minority",
+        k_smote: int = 5,
+        k_enn: int = 3,
+        kind_sel_enn: str = "all",
+        random_state: int = 0,
+        scaler: Any = StandardScaler(with_mean=True, with_std=True),
+    ) -> None:
         """Instantiate the class."""
         self.set_params(
             **{
@@ -105,13 +108,13 @@ class ScaledSMOTEENN:
             }
         )
 
-    def set_params(self, **parameters):
+    def set_params(self, **parameters: Any) -> "ScaledSMOTEENN":
         """Set parameters; for consistency with scikit-learn's estimator API."""
         for parameter, value in parameters.items():
             setattr(self, parameter, value)
         return self
 
-    def get_params(self, deep=True):
+    def get_params(self, deep: bool = True) -> dict[str, Any]:
         """Get parameters; for consistency with scikit-learn's estimator API."""
         return {
             "sampling_strategy_smote": self.sampling_strategy_smote,
@@ -123,7 +126,13 @@ class ScaledSMOTEENN:
             "scaler": self.scaler,
         }
 
-    def fit_resample(self, X, y):
+    def fit_resample(
+        self,
+        X: NDArray[np.floating],
+        y: Union[NDArray[np.integer], NDArray[np.str_]],
+    ) -> tuple[
+        NDArray[np.floating], Union[NDArray[np.integer], NDArray[np.str_]]
+    ]:
         """
         Resample the dataset.
 
@@ -138,15 +147,14 @@ class ScaledSMOTEENN:
         Returns
         -------
         X_resampled : ndarray(float, ndim=2)
-            Resampled X and y in the original "units" of X.
+            Resampled `X` and `y` in the original "units" of `X`.
 
         y_resampled : ndarray(str or int, ndim=1)
-            Classes associated with each row in X.
+            Classes associated with each row in `X`.
 
         Note
         ----
-        First standardize X, then perform SMOTEENN, then de-standardize
-        to return results in the same "units" as input.
+        First standardize `X`, then perform SMOTEENN, then de-standardize to return results in the same "units" as input.
         """
         X_std = self.scaler.fit_transform(X)
 
