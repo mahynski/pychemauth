6f385cd79665fae8929162b608664ae040f34c44
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-11-25 16:23:38 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,10 +32,12 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+github_url = "https://github.com/mahynski/pychemauth/"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_logo = "_static/logo_no.png"
 html_theme = "sphinx_book_theme"  #'sphinx_rtd_theme'
 html_static_path = ["_static"]
 pygments_style = "sphinx"
